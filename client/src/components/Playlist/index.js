08e7cc003e2a4cc93d9e98a897eb33a47b5cb6bb
John Babin <johnebabinjr@gmail.com>
2019-11-18 21:37:43 -0800

    working on playlist

--- client/src/components/Playlist/index.js
+++ client/src/components/Playlist/index.js
@@ -1,20 +1,58 @@
-import React from "react";
+// import React from "react";
+// import axios from "axios";
 
-function Playlist() {
-  return (
-    <div className="playlist">
-      <form>
-        <input
-          type="text"
-          placeholder="Album"
-          name="tempAlbum"
-          value={this.state.tempAlbum}
-          onChange={this.handleInputChange}
-        />
-        <button onClick={this.handleFormSubmit}>Submit</button>
-      </form>
-    </div>
-  );
-}
+// // const Playlist= props => {
+// //   API.getTracksByRoomId(props.currentRoom).then(function(res){
+// //     for (var i =0;i<res.length<i++)
+// //   return (
+// //     <div className="">
+// //       <table></table>
+// //     </div>
+// //   );
+// // }
 
-export default Playlist;
+// let Playlist = props => {
+//   getTracksByRoomId: function(props.currentRoom) {
+//     return axios.get("/api/tracks/" + id);
+//   },
+
+//   return (
+//     <div>
+//       <select
+//         value={props.currentRoom}
+//         name="exp_year"
+//         className="form-control"
+//         onChange="this.handleInputChange"
+//       ></select>
+//     </div>
+//   );
+// };
+
+// render() {
+//   const options = [];
+
+//   for (let i = 2017; i <= 2050; i++) {
+//     options.push(<option value={i}>{i}</option>);
+//   }
+
+// return (
+//   <div>
+//     <select
+//       value={this.state.exp_year}
+//       name="exp_year"
+//       className="form-control"
+//       onChange="this.handleInputChange"
+//     >
+//       <option value="">===Select Expiry Year===</option>
+//       {options}
+//     </select>
+//   </div>
+// );
+
+// export default Playlist;
+
+//TO DO
+// set songs to state and pass in here to turn into a prop
+// map the songs array and render them
+// set if styling statement to change based on if song is played
+// render out as a table
