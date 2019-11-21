54f960301d19359e18fb4f012e6b2a54b7a3fb6a
John Babin <johnebabinjr@gmail.com>
2019-11-20 21:45:26 -0800

    making playlist work

--- models/track.js
+++ models/track.js
@@ -3,14 +3,14 @@ const autoIncrement = require("mongoose-auto-increment");
 const Schema = mongoose.Schema;
 
 const spotifySchema = new Schema({
-  roomId: { type: String, required: true, unique: true },
+  roomId: { type: String, required: true },
   trackId: { type: String, required: true },
   trackName: { type: String, required: true },
   artistName: { type: String, required: true },
   albumName: { type: String, required: true },
   albumCover: { type: String, required: true },
   userName: { type: String, required: true },
-  played: { type: Boolean, required: true },
+  played: { type: Boolean, required: true, default: false },
   date: { type: Date, default: Date.now }
 });
 
